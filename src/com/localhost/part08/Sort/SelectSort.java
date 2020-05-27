6d9b45a728898c6e7e5e90ec507b924874d2ff24
shuaishuai <1677322785@qq.com>
2020-05-27 09:26:38 +0800

    tu

---
+++ src/com/localhost/part08/Sort/SelectSort.java
@@ -0,0 +1,27 @@
+package com.localhost.part08.Sort;
+
+/**
+ * Created by HP on 2020/5/26.
+ */
+public class SelectSort {
+
+
+    public static void selectionSort(int[] array) {
+        int n = array.length;
+        int min = 999;
+        int temp;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if(array[j] < min){
+
+                }
+            }
+
+        }
+    }
+
+    public static void main(String[] args){
+
+    }
+
+}
